3424ac66dacec9fedc350b4291899c7a086e55f2
Goga <2doug077@gmail.com>
2022-01-11 07:30:53 -0300

    Project created

---
+++ backend/src/main/java/com/douglasoliveira/projectmovie/ProjectmovieApplication.java
@@ -0,0 +1,13 @@
+package com.douglasoliveira.projectmovie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectmovieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectmovieApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/douglasoliveira/projectmovie/ProjectmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.douglasoliveira.projectmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
